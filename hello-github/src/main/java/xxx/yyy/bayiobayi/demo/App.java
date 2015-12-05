097349f009a5f7d68f57312eb3d8591be6e06663
terje <terje@terje-pc>
2015-12-05 22:15:01 +0800

    reset15

--- hello-github/src/main/java/xxx/yyy/bayiobayi/demo/App.java
+++ hello-github/src/main/java/xxx/yyy/bayiobayi/demo/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!!!! reset14" );
+        System.out.println( "Hello World!!!! reset15" );
         System.out.println("hello github");
         System.out.println("$$$$$$$");
         System.out.println("%%%%%%%%%@@@@@@@@@#####");
