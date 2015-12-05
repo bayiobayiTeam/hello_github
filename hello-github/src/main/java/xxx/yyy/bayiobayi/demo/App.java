1118914c4ee44561012c6b990494ed5e2978cf02
terje <terje@terje-pc>
2015-12-05 22:34:47 +0800

    reset19

--- hello-github/src/main/java/xxx/yyy/bayiobayi/demo/App.java
+++ hello-github/src/main/java/xxx/yyy/bayiobayi/demo/App.java
@@ -9,7 +9,7 @@ public class App
 	//stash回来如果有冲突，可以采用 remove from index
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!!!! reset17" );
+        System.out.println( "Hello World!!!! reset19" );
         System.out.println("hello github");
         System.out.println("$$$$$$$");
         System.out.println("%%%%%%%%%@@@@@@@@@#####");
