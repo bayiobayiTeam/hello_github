51dc51c4a687fc0498fb69390a41744a2bbee7bf
terje <terje@localhost.localdomain>
2015-12-05 17:54:57 +0800

    $$$$

--- hello-github/src/main/java/xxx/yyy/bayiobayi/demo/App.java
+++ hello-github/src/main/java/xxx/yyy/bayiobayi/demo/App.java
@@ -11,5 +11,6 @@ public class App
         System.out.println( "Hello World!!!!" );
         System.out.println("hello github");
         System.out.println("$$$$$$$");
+        System.out.println("%%%%%%%%%");
     }
 }
