4a92a396d38ab68cd6d65059604d3f4c00c322b7
terje <terje@terje-pc>
2015-12-06 17:07:41 +0800

    add app

---
+++ hello-github/src/main/java/xxx/yyy/bayiobayi/demo/App.java
@@ -0,0 +1,23 @@
+package xxx.yyy.bayiobayi.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	//stash回来如果有冲突，即便在文件中将冲突直接修复，也没法提交。
+	//可以采用 remove from index
+    public static void main( String[] args )
+    {
+        System.out.println( "4comflict~~~  ###comflict!!!!!!!" );
+        System.out.println( "Hello World!!!! pusht to upstream @@@" );
+        System.out.println( "Hello World!!!! reset23 @@@" );
+        System.out.println("hello github");
+        System.out.println("*************8");
+        System.out.println("$$$$$$$");
+        System.out.println("%%%%%%%%%@@@@@@@@@#####");
+        
+        System.out.println("add sth");
+    }
+}
