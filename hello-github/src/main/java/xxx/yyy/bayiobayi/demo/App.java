70994944f3a8fe73a52bb62dc6a306e17b9879c9
terje <terje@localhost.localdomain>
2015-12-06 15:04:20 +0800

    @@@

--- hello-github/src/main/java/xxx/yyy/bayiobayi/demo/App.java
+++ hello-github/src/main/java/xxx/yyy/bayiobayi/demo/App.java
@@ -10,8 +10,8 @@ public class App
 	//可以采用 remove from index
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!!!! pusht to upstream" );
-        System.out.println( "Hello World!!!! reset23" );
+        System.out.println( "Hello World!!!! pusht to upstream @@@" );
+        System.out.println( "Hello World!!!! reset23 @@@" );
         System.out.println("hello github");
         System.out.println("*************8");
         System.out.println("$$$$$$$");
