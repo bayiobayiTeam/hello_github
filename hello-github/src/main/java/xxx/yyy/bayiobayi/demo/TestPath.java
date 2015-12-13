37c6e6b3641ebc76c6ee2fa1f6d1d81f1d892381
terje <terje@terje-pc>
2015-12-13 21:40:37 +0800

    patch06

--- hello-github/src/main/java/xxx/yyy/bayiobayi/demo/TestPath.java
+++ hello-github/src/main/java/xxx/yyy/bayiobayi/demo/TestPath.java
@@ -21,6 +21,10 @@ public class TestPath
 		System.out.println("test patch003");
 		
 		System.out.println("test patch004");
+		
+		System.out.println("test patch005");
+		
+		System.out.println("test patch006");
 
 	}
 
