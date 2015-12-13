7ec4dfd73c47ae0a5c53d0e355172770756a986b
terje <terje@terje-pc>
2015-12-13 21:20:02 +0800

    333 patch

---
+++ hello-github/src/main/java/xxx/yyy/bayiobayi/demo/TestPath.java
@@ -0,0 +1,13 @@
+package xxx.yyy.bayiobayi.demo;
+
+public class TestPath 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("++++++++++");
+		System.out.println("patch");
+		System.out.println("++++++++++");
+
+	}
+
+}
